b8cd4622f7cc84aa977b74db28060275bed57078
Endre Vegh <endre.vegh@yahoo.com>
2020-06-01 20:06:40 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+
+exports.handler = async () => {
+  var fetchResponse = await fetch(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
+
+  if (fetchResponse.ok) {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(await fetchResponse.json()),
+    };
+    return response;
+  }
+
+  const response = {
+    statusCode: 500,
+    body: JSON.stringify("Something went wrong."),
+  };
+
+  return response;
+};
